6ab8201ea644219dae6170fbb27a3e120d4356dc
Romeo <51507372+robeng1@users.noreply.github.com>
2021-12-15 08:51:49 +0000

    minor fix

--- src/main/java/com/streets/marketsvc/mds/data/services/ExchangeDataServiceImpl.java
+++ src/main/java/com/streets/marketsvc/mds/data/services/ExchangeDataServiceImpl.java
@@ -10,6 +10,7 @@ import com.streets.marketsvc.utils.PropertiesReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -82,7 +83,7 @@ public class ExchangeDataServiceImpl implements ExchangeDataService {
         this.cleanAndSave(xchangeDataList, xchange);
     }
 
-    @PostConstruct
+    @Scheduled(initialDelay = 10, fixedDelay = 100)
     public void readXchangeState() {
         RestTemplate restTemplate = new RestTemplate();
         this.readXchangeOneState(restTemplate);
